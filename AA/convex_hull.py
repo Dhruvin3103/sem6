d4e9c85530409df1cb40e365c46c8b4146a79763
Dhruvin3103 <dcchawda2003@gmail.com>
2024-04-12 14:28:26 +0530

    -_-

---
+++ AA/convex_hull.py
@@ -0,0 +1,23 @@
+import math 
+def structure(p, q, r): 
+    v = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]) 
+    if v == 0: 
+        return 0  
+    return 1 if v > 0 else 2 
+def gs(points): 
+    n = len(points) 
+    if n < 3: 
+        return [] 
+    min_pt = min(points, key=lambda x: (x[1], x[0])) 
+    sort_pts = sorted(points, key=lambda x: (math.atan2(x[1] - min_pt[1], x[0] - min_pt[0]), 
+x)) 
+    stack = [sort_pts[0], sort_pts[1], sort_pts[2]] 
+    print(f"After addition 3 points , stack : {stack}") 
+    for i in range(3, n): 
+        while len(stack) > 1 and structure(stack[-2], stack[-1], sort_pts[i]) != 2: 
+            stack.pop() 
+        stack.append(sort_pts[i]) 
+        print(f"adding {sort_pts[i]} --> stack : {stack}") 
+    return stack 
+points =[(0, 0), (3, 1), (1, 4), (3, 3), (5, 2), (5, 5), (9, 6), (7, 0)]
+print(f"Convex hull will be : {gs(points) }") 
---
+++ AA/fulkerson.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+def ford_fulkerson(G, source, sink):
+    max_flw = 0
+    allpath,path_flws = [],[]
+    while True:
+        paths = bfs(G, source, sink)
+        if not paths:
+            break
+        for path in paths:
+            path_flw = min(G[path[i]][path[i+1]] for i in range(len(path)-1))
+            allpath.append(path)
+            path_flws.append(path_flw)
+            max_flw += path_flw
+            G = modify_G(G, path, path_flw)
+    return max_flw, allpath, path_flws
+def bfs(G, source, sink):
+    paths = []
+    queue = deque([(source, [source])])
+    while queue:
+        u, path = queue.popleft()
+        for v, capacity in enumerate(G[u]):
+            if capacity > 0 and v not in path:
+                if v == sink:
+                    paths.append(path + [v])
+                else:
+                    queue.append((v, path + [v]))
+    return paths
+def modify_G(G, path, flw):
+    for i in range(len(path)-1):
+        u, v = path[i], path[i+1]
+        G[u][v] -= flw
+        G[v][u] += flw  
+    return G
+G = [[0, 16, 13, 0, 0, 0],
+         [0, 0, 10, 12, 0, 0],
+         [0, 4, 0, 0, 14, 0],
+         [0, 0, 9, 0, 0, 20],
+         [0, 0, 0, 7, 0, 4],
+         [0, 0, 0, 0, 0, 0]]
+source = 0
+sink = 5
+max_flw, allpath, path_flws = ford_fulkerson(G, source, sink)
+print(f"Maximum flow: {max_flw}")
+print("<-----All augmenting paths and their corresponding flows----->")
+for i, path in enumerate(allpath):
+    print(f"Path --> {path}, flow along path: {path_flws[i]}")
