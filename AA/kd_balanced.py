98acb3f31e5b1abd1710b3e6531e954904506afd
Dhruvin3103 <dcchawda2003@gmail.com>
2024-05-07 12:20:56 +0530

    >

--- AA/kd_balanced.py
+++ AA/kd_balanced.py
@@ -7,18 +7,23 @@ def build_kdtree(points, depth=0):
     points.sort(key=lambda x: x[axis]) # sort krna hai based on the axis so agar 1st level hai to x ke basis fir 2nd pe y, 3rd pe x (but agar k=3 to z ke basis pe sort hoga )
     median = len(points) // 2 # middle postion find kr raha 
     
-    return (points[median], axis, 
-            build_kdtree(points[:median], depth + 1), 
-            build_kdtree(points[median + 1:], depth + 1)) # recursive call kr rhaa hai and return kr rha where 1st is the current point, 2nd  is level, 3rd is left wala tree, 4th is right tree, ye aise isliye kiya taki print barabr kr sake print_tree func dekho clear hoga aisa return kyu kiya 
+    return {
+        'point': points[median],  # Current point
+        'axis': axis,  # Axis for splitting
+        'left': build_kdtree(points[:median], depth + 1),  # Recursively build left subtree
+        'right': build_kdtree(points[median + 1:], depth + 1)  # Recursively build right subtree
+    }# recursive call kr rhaa hai and return kr rha where 1st is the current point, 2nd  is level, 3rd is left wala tree, 4th is right tree, ye aise isliye kiya taki print barabr kr sake print_tree func dekho clear hoga aisa return kyu kiya 
+
 
 def print_tree(node, level=0, side=None):
     if node is not None:
-        prefix = "" 
-        if side is not None: # ye side ke saath bata ta 
-            prefix = side + "---- " # ye har new level pe hoga 
-        print("   " * level + prefix + str(node[0])) # print point with left ki righht hai wo bhi batayega 
-        print_tree(node[2], level + 1, "L") 
-        print_tree(node[3], level + 1, "R") 
+        prefix = ""
+        if side is not None:
+            prefix = side + "---- "  # Add prefix to indicate left or right child
+        print("   " * level + prefix + str(node['point']))  # Print the point
+        # Recursively print left and right subtrees
+        print_tree(node['left'], level + 1, "L")
+        print_tree(node['right'], level + 1, "R")
 
 points = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
 root = build_kdtree(points)
--- AA/kd_unbalanced.py
+++ AA/kd_unbalanced.py
@@ -1,28 +1,23 @@
-class Node: 
-    def __init__(self, point, axis): 
-        self.point = point 
-        self.axis = axis 
-        self.left = None 
-        self.right = None 
- 
 def insert(root, point, axis=0): 
+    # agar root nhi hai to return the following ye tabhi aa sakta when root is initally na diya ho ya fir leaf nodes pe poch gye ho tab 
     if root is None: 
-        return Node(point, axis) 
-    if point[axis] < root.point[axis]: 
-        root.left = insert(root.left, point, (axis + 1) % len(point)) 
+        return {"point": point, "axis": axis, "left": None, "right": None}
+    # unbalanced me hum current point ko har level se compare krte hai aur jaha condition match hoti waha attach kr dete wo yaha ho rha 
+    if point[axis] < root["point"][axis]: 
+        root["left"] = insert(root["left"], point, (axis + 1) % len(point)) 
     else: 
-        root.right = insert(root.right, point, (axis + 1) % len(point)) 
+        root["right"] = insert(root["right"], point, (axis + 1) % len(point)) 
     return root 
  
 def print_tree(node, level=0, side=None): 
+    # print kr rha hai balanced me jaise kr rha waise hi idar bhi ho rha 
     if node is not None: 
         prefix = "" 
         if side is not None: 
             prefix = side + "---- " 
-        print("   " * level + prefix + str(node.point)) 
-        print_tree(node.left, level + 1, "L") 
-        print_tree(node.right, level + 1, "R") 
- 
+        print("   " * level + prefix + str(node["point"])) 
+        print_tree(node["left"], level + 1, "L") 
+        print_tree(node["right"], level + 1, "R") 
 
 points = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
 
