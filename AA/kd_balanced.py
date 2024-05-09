da2152ce39747671da602e81fabb00086a115dbd
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2024-05-09 18:30:54 +0530

    Update kd_balanced.py

--- AA/kd_balanced.py
+++ AA/kd_balanced.py
@@ -14,5 +14,4 @@ def print_tree(root,level=0,side=None):
     print_tree(root['left'],level+1,'L')
     print_tree(root['right'],level+1,'R')
 points = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
-root = None 
 print_tree(buildKdtree(points))
