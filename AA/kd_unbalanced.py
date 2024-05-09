4176dc742c42e99edff62bf98d4c49a978e24b06
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2024-05-09 18:30:06 +0530

    Update kd_unbalanced.py

--- AA/kd_unbalanced.py
+++ AA/kd_unbalanced.py
@@ -1,16 +1,12 @@
 def insert(root, point, axis=0): 
-    # agar root nhi hai to return the following ye tabhi aa sakta when root is initally na diya ho ya fir leaf nodes pe poch gye ho tab 
     if root is None: 
         return {"point": point, "axis": axis, "left": None, "right": None}
-    # unbalanced me hum current point ko har level se compare krte hai aur jaha condition match hoti waha attach kr dete wo yaha ho rha 
     if point[axis] < root["point"][axis]: 
         root["left"] = insert(root["left"], point, (axis + 1) % len(point)) 
     else: 
         root["right"] = insert(root["right"], point, (axis + 1) % len(point)) 
-    return root 
- 
+    return root  
 def print_tree(node, level=0, side=None): 
-    # print kr rha hai balanced me jaise kr rha waise hi idar bhi ho rha 
     if node is not None: 
         prefix = "" 
         if side is not None: 
@@ -18,13 +14,8 @@ def print_tree(node, level=0, side=None):
         print("   " * level + prefix + str(node["point"])) 
         print_tree(node["left"], level + 1, "L") 
         print_tree(node["right"], level + 1, "R") 
-
 points = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
-
 root = None 
-
-# idar har point pe wo function me ja rha fir root ka dict update hoga 
 for point in points: 
-    root = insert(root, point,0) 
-
+    root = insert(root,point,0) 
 print_tree(root)
