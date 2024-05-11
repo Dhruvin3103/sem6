75beb61b449d6597aafd2807cd6793de97b91ed3
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2024-05-12 00:25:46 +0530

    Update vigrenere.py

--- IS/vigrenere.py
+++ IS/vigrenere.py
@@ -6,15 +6,9 @@ if len(plain_txt) == len(key):
 else:
     for i in range(len(plain_txt)-len(key)):
         padd_key += key[i%len(key)]
-
-print(f"\nPlain Text : {plain_txt}\nKey : {key}\nPadded Key : {padd_key}\n")
-print('encrption : ')
-encrpyted = ""
+encrpyted,decrpyted = "",""
 for i in range(len(plain_txt)):
     encrpyted += chr(((ord(plain_txt[i]) + ord(padd_key[i])) % 26)+65)
-print(f"after encrption, cipher text : {encrpyted}")
-print('decyprtion : ')
-decrpyted = ""
 for i in range(len(plain_txt)):
     decrpyted += chr(((ord(encrpyted[i]) - ord(padd_key[i])) % 26)+65)
-print(f"after decyprtion, decrypted text : {decrpyted}")
+print(f"\nPlain Text : {plain_txt}\nKey : {key}\nPadded Key : {padd_key}\nafter encrption, cipher text : {encrpyted}\nafter decyprtion, decrypted text : {decrpyted}")
