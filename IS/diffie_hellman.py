96a111ff8f842435aa5da684ccc54a4532011869
Dhruvin Chawda <114014218+Dhruvin3103@users.noreply.github.com>
2024-05-17 13:09:17 +0530

    Update diffie_hellman.py

--- IS/diffie_hellman.py
+++ IS/diffie_hellman.py
@@ -7,23 +7,24 @@ def is_prime(n):
     return True
 def isPrimitiveRoot(g,p):
     listt=[pow(g,i,p) for i in range(p-1)]
-    for i in listt:
-        if listt.count(i)>1:
-            return 0
-    return 1 
-while True:
-    p=int(input("Enter values of p:"))
-    g=int(input("Enter values of g:"))
-    if (is_prime(p) and isPrimitiveRoot(g,p)):
-        a=int(input("Enter values of a:"))
-        b=int(input("Enter values of b:"))
-        if a >= p or b >= p:
-            print(f"Private Key Of Both The Users Should Be Less Than {p}!")
-        else:
-            XA,XB=(g**a) % p  , (g**b) % p
-            AK,BK = (XB**a) % p  , (XA**b) % p
-            print(f"\nSecret Key For User 1 Is {AK}\nSecret Key For User 2 Is {BK}\n")
-            print("Keys Have Been Exchanged Successfully") if AK == BK else print("Keys Have Not Been Exchanged Successfully")
-            break    
+    unique = list(set(listt))
+    if len(listt) == len(unique):
+        return True
+    return False
+
+p=int(input("Enter values of p:"))
+g=int(input("Enter values of g:"))
+if (is_prime(p) and isPrimitiveRoot(g,p)):
+    a=int(input("Enter values of a:"))
+    b=int(input("Enter values of b:"))
+    if a >= p and b >= p:
+        print(f"Private Key Of Both The Users Should Be Less Than {p}!")
     else:
-        print("enter correct values")
+        XA,XB=(g**a) % p  , (g**b) % p
+        AK,BK = (XB**a) % p  , (XA**b) % p
+        print(f"XA : {XA}, XB : {XB}")
+        print(f"\nSecret Key For User 1 Is {AK}\nSecret Key For User 2 Is {BK}\n")
+        print("Keys Have Been Exchanged Successfully") if AK == BK else print("Keys Have Not Been Exchanged Successfully")
+        # break    
+else:
+    print("enter correct values")
