ea4151e125464cbd02823f1db8215b7a64167629
Dhruvin Chawda <114014218+Dhruvin3103@users.noreply.github.com>
2024-05-17 13:49:35 +0530

    Update vernam.py

--- IS/vernam.py
+++ IS/vernam.py
@@ -4,5 +4,12 @@ def encDec(word,key):
         res=(ord(word[i])-97)^(ord(key[i])-97)
         result.append(chr((res)+97))
     return ''.join(result)
-result=encDec(word='lifeisnoteasy',key='youneverknoww')
-print(f'Encrypted:{result}\nDecrypted:{encDec(result,key="youneverknoww")}')
+pt = "lifeisnoteasy"
+key = "youneverknow"
+pad_key = key
+if len(pt) != len(key):
+    for i in range(len(pt) - len(key)):
+        pad_key += key[i % len(key)]
+print(f"Key used is : {pad_key}")
+result=encDec(word=pt,key=pad_key)
+print(f'Encrypted:{result}\nDecrypted:{encDec(result,key=pad_key)}')
